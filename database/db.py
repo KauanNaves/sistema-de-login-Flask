ab769d30ae6adbbc4d0da9f8182a92248655083a
Kauan Augusto Naves <kauannazanaves0@gmail.com>
2026-06-03 10:48:21 -0300

    Criado estrutura inicial de arquivos para implementação do sqlite
    Criado a estrutura inicial de arquivos para implementação do sqlite3 como Banco de Dados.

---
+++ database/db.py
@@ -0,0 +1,18 @@
+# Implementando Banco de Dados do Sistema de Login usando sqlite3
+
+import sqlite3
+
+# Criar banco de dados
+
+
+# Criar tabela users se não existir
+
+
+# Criar usuário
+
+
+# Buscar usuário por id
+
+
+# Deletar usuário
+
